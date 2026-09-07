5004deebedc7ae0fe5527d26c7d57457ef6bf7c2
killjoy00 <killjoy00@yahoo.com>
2026-09-07 09:25:09 -0500

    Install human copy layer

--- bootstrap.mjs
+++ bootstrap.mjs
@@ -2,11 +2,13 @@ const params = new URLSearchParams(window.location.search);
 const challengeMode = params.has('challenge');
 const product = await import('./product.mjs');
 const flow = await import('./flow-fixes.mjs');
+const humanCopy = await import('./human-copy.mjs');
 
 if (challengeMode) {
   await import('./social.mjs');
   product.installProductLayer();
   flow.installFlowFixes();
+  humanCopy.installHumanCopy();
 } else {
   const seed = params.get('seed');
   if (seed) product.seedGameRandom(seed);
@@ -14,4 +16,5 @@ if (challengeMode) {
   await import('./social.mjs');
   product.installProductLayer();
   flow.installFlowFixes();
+  humanCopy.installHumanCopy();
 }
