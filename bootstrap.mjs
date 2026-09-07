2b121c10f7324fff231df9b208861812f600e06a
killjoy00 <killjoy00@yahoo.com>
2026-09-06 23:58:07 -0500

    Install result flow fixes

--- bootstrap.mjs
+++ bootstrap.mjs
@@ -1,14 +1,17 @@
 const params = new URLSearchParams(window.location.search);
 const challengeMode = params.has('challenge');
 const product = await import('./product.mjs');
+const flow = await import('./flow-fixes.mjs');
 
 if (challengeMode) {
   await import('./social.mjs');
   product.installProductLayer();
+  flow.installFlowFixes();
 } else {
   const seed = params.get('seed');
   if (seed) product.seedGameRandom(seed);
   await import('./app.js');
   await import('./social.mjs');
   product.installProductLayer();
+  flow.installFlowFixes();
 }
