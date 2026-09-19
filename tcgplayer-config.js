99c6d5b5fecc561e2c50679e7ffd3dd2388e0e34
killjoy00 <killjoy00@yahoo.com>
2026-09-19 18:30:53 -0500

    Activate approved TCGplayer affiliate routing

--- tcgplayer-config.js
+++ tcgplayer-config.js
@@ -1,8 +1,7 @@
 // Public configuration only. Do not put API secrets here.
 window.PACKONE_TCGPLAYER = Object.freeze({
   program: 'Impact',
-  // After TCGplayer/Impact approval, paste the deep-link template supplied by Impact.
-  // It must contain {url}; Pack One will insert the encoded TCGplayer destination.
-  impactDeepLinkTemplate: '',
+  // Approved TCGplayer/Impact referral route. {url} is replaced with the encoded card-search destination.
+  impactDeepLinkTemplate: 'https://partner.tcgplayer.com/c/7742974/1780961/21018?u={url}',
   disclosure: 'Pack One may earn a commission from eligible purchases made through TCGplayer links.'
 });
