b80e815e12dc22a05720e8aefec60edf53d1cd25
killjoy00 <killjoy00@yahoo.com>
2026-09-19 21:33:25 -0500

    Feature-gate first-party API cutover

--- leaderboard-config.js
+++ leaderboard-config.js
@@ -1,5 +1,6 @@
 // Public Pack 1 API endpoints. Ranked scoring/leaderboards stay isolated from growth/profile services.
 window.PACK1_API = {
+  firstParty: false,
   url: 'https://br-orange-feather-ayps8kep-pack1api.compute.c-5.us-east-2.aws.neon.tech',
   growthUrl: 'https://br-orange-feather-ayps8kep-pack1growth.compute.c-5.us-east-2.aws.neon.tech',
   draftRunUrl: 'https://br-orange-feather-ayps8kep-draftrunapi.compute.c-5.us-east-2.aws.neon.tech',
