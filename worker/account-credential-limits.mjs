754914af72d4e50f8a343bbb661df1047937b94a
killjoy00 <killjoy00@yahoo.com>
2026-09-21 12:06:49 -0500

    fix(auth): allow account deletion limiter purposes

--- worker/account-credential-limits.mjs
+++ worker/account-credential-limits.mjs
@@ -3,6 +3,9 @@ import {createHmac,timingSafeEqual} from 'node:crypto';
 const PURPOSES=new Set([
   'current_password',
   'password_change_network',
+  'account_delete_verify',
+  'account_delete_network',
+  'account_delete_init',
 ]);
 const UUID=/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 const DIGEST=/^[a-f0-9]{64}$/;
