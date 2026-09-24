bb2f03aa2c3f0bd683df1774153cd40655e87c10
killjoy00 <killjoy00@yahoo.com>
2026-09-24 07:35:41 -0500

    Fix safe-area Home import

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,5 @@
 import { router } from 'expo-router';
-import { Pressable, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
