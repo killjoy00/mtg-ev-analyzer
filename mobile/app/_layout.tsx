0702805ad7622392f5807620c7cc2263651a5883
killjoy00 <killjoy00@yahoo.com>
2026-09-25 13:40:19 -0500

    Tidy version-gated root layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout() {
     <VersionGate>
       <>
         <StatusBar style="dark" />
-      <Stack
+        <Stack
         unstable_screenErrorBoundary={ScreenErrorBoundary}
         screenOptions={{
           headerStyle: { backgroundColor: colors.surface },
@@ -18,17 +18,17 @@ export default function RootLayout() {
           headerTintColor: colors.ink,
           contentStyle: { backgroundColor: colors.page },
         }}
-      >
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="draft-run" options={{ title: 'Draft Run' }} />
-        <Stack.Screen name="practice" options={{ title: 'Practice' }} />
-        <Stack.Screen name="leaderboard" options={{ title: 'Leaderboard' }} />
-        <Stack.Screen name="career" options={{ title: 'Career' }} />
-        <Stack.Screen name="how-to" options={{ title: 'How to Play' }} />
-        <Stack.Screen name="scoring" options={{ title: 'Scoring' }} />
-        <Stack.Screen name="method" options={{ title: 'Method' }} />
-        <Stack.Screen name="sets" options={{ title: 'Sets' }} />
-        <Stack.Screen name="account" options={{ title: 'Account' }} />
+        >
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="draft-run" options={{ title: 'Draft Run' }} />
+          <Stack.Screen name="practice" options={{ title: 'Practice' }} />
+          <Stack.Screen name="leaderboard" options={{ title: 'Leaderboard' }} />
+          <Stack.Screen name="career" options={{ title: 'Career' }} />
+          <Stack.Screen name="how-to" options={{ title: 'How to Play' }} />
+          <Stack.Screen name="scoring" options={{ title: 'Scoring' }} />
+          <Stack.Screen name="method" options={{ title: 'Method' }} />
+          <Stack.Screen name="sets" options={{ title: 'Sets' }} />
+          <Stack.Screen name="account" options={{ title: 'Account' }} />
         </Stack>
       </>
     </VersionGate>
